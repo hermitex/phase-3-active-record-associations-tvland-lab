88f0e8b25876740060e3ac7670c0843a494284e2
Tiberius Mairura <tiberiusmairura.dev@gmail.com>
2022-09-02 19:11:48 +0300

    commit tree 98781b5e428d45dacc1af93668c69b5ae1460ae8

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,3 +1,15 @@
 class Character < ActiveRecord::Base
-  
+  belongs_to :show
+  belongs_to :actor
+  def actor
+    Actor.find_by(id: self.actor_id)
+  end
+
+  def show
+    Show.find_or_create_by(id: self.id)
+  end
+
+  def say_that_thing_you_say
+    "#{self[:name]} always says: #{self[:catchphrase]}"
+  end
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,17 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+  def characters
+    Character.all
+  end
+
+  def network
+    Network.create(id: nil, call_letters: "NBC")
+    Network.all
+  end
+
+  def actors_list
+   Actor.all.map { |actor| actor[:first_name] << " " << actor[:last_name]  }
+  end
 end
